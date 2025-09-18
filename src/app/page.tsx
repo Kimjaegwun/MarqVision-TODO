79e14b27112dcc7c12317ae9daf6d46455def194
JaeGwun <rlawornjs4@naver.com>
2025-09-18 16:21:30 +0900

    feat: layout design and input component set

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,32 @@
+"use client";
+
+import { useState } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const [tasks, setTasks] = useState<string[]>([]);
+  const [task, setTask] = useState<string>("");
+
+  const handleAddTask = () => {
+    setTasks([...tasks, task]);
+    setTask("");
+  };
+
   return (
-    <div className={styles.page}>
-    
+    <div className={styles.container}>
+      <h1>Marq Todo</h1>
+      <div className={styles["input-wrapper"]}>
+          <input
+            className={styles.input}
+            type="text"
+            placeholder="Add a new task"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+          />
+          <button className={styles.button} onClick={handleAddTask}>
+            Add
+          </button>
+      </div>
     </div>
   );
 }
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,5 +1,7 @@
+import styles from "./index.module.css";
+
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return <div className={styles.layout}>{children}</div>;
 };
 
 export default Layout;
